8668019a32240aa94e6ac4fec9bc4332e02b0f9e
Maciej Synowski <34197298+Shizerq@users.noreply.github.com>
2021-04-06 22:51:54 +0200

    Create Card component

---
+++ components/Card/index.styled.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 150px;
+  height: 150px;
+
+  border-radius: 10px;
+
+  background: #260c1a;
+
+  padding-horizontal: 10px;
+  padding-vertical: 5%;
+
+  justify-content: space-between;
+`;
+
+export const Name = styled.Text`
+  font-size: 18px;
+  font-weight: 700;
+  color: #ffffff;
+`;
+
+export const Label = styled.Text`
+  color: #ffe81f;
+  font-weight: 600;
+`;
+
+export const Data = styled.Text`
+  color: #ffffff;
+  text-transform: capitalize;
+`;
---
+++ components/Card/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text } from "react-native";
+import * as Styled from "./index.styled";
+
+interface Props {
+  name: string;
+  gender: "Female" | "Male" | "unknown" | "n/a";
+  birthyear: string;
+}
+
+export const Card: React.FC<Props> = ({ name, gender, birthyear }) => {
+  return (
+    <Styled.Container>
+      <Styled.Name>{name}</Styled.Name>
+      <Text>
+        <Styled.Label>Gender: </Styled.Label>
+        <Styled.Data>{gender}</Styled.Data>
+      </Text>
+      <Text>
+        <Styled.Label>Birth year: </Styled.Label>
+        <Styled.Data>{birthyear}</Styled.Data>
+      </Text>
+    </Styled.Container>
+  );
+};
+
+export default Card;
