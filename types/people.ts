958fce02fd556b3805b430b8ad44e17e97e3c41d
Maciej Synowski <34197298+Shizerq@users.noreply.github.com>
2021-04-07 17:10:40 +0200

    Add People types

---
+++ types/people.ts
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+export default interface People {
+  birth_year: string;
+  created: string;
+  edited: string;
+  eye_color: string;
+  films: string[];
+  gender: "Female" | "Male" | "unknown" | "n/a";
+  hair_color: string;
+  height: string;
+  homeworld: string;
+  mass: string;
+  name: string;
+  skin_color: string;
+  species: string[];
+  starships: string[];
+  url: string;
+  vehicles: string[];
+}
