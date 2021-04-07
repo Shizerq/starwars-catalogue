62ca800e136a32dea269fcbabb64dd33c52ad7ff
Maciej Synowski <34197298+Shizerq@users.noreply.github.com>
2021-04-07 17:11:47 +0200

    Add Navigation to the App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from "expo-status-bar";
+/* eslint-disable react/style-prop-object */
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-export default function App() {
+import { NavigationContainer, RouteProp } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import HomeScreen from "./screens/Home";
+import DetailsScreen from "./screens/Details";
+
+import People from "./types/people";
+
+export type Params = {
+  Details: People;
+
+  Home: undefined;
+};
+
+export type DetailsParams = RouteProp<Params, "Details">;
+
+const Stack = createStackNavigator<Params>();
+const queryClient = new QueryClient();
+
+export const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Details"
+            component={DetailsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
